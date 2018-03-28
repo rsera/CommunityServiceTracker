88b0f3a67da47eee904e19ca295e0f41658e5cab
ksiler <ksiler7@knights.ucf.edu>
2018-03-28 09:00:29 -0400

    Removed and created a new ReactNative folder

--- ReactNative/vtrak/android/app/src/main/java/com/vtrak/MainApplication.java
+++ ReactNative/vtrak/android/app/src/main/java/com/vtrak/MainApplication.java
@@ -1,4 +1,4 @@
-package com.albums;
+package com.vtrak;
 
 import android.app.Application;
 
--- ReactNative/vtrak/index.js
+++ ReactNative/vtrak/index.js
@@ -1,14 +1,4 @@
-// Import a library to help create a component
-import React from 'react';
 import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import App from './App';
 
-// Create a component
-const App = () => {
-  return (
-    <Header/>
-  );
-};
-
-// Render it to the device
-AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent('vtrak', () => App);
