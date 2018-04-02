831957b6e04e2e7ce236ac6ec1ada6518387c05a
ksiler <ksiler7@knights.ucf.edu>
2018-04-02 14:50:04 -0400

    Created VolunteerEventList Class

--- ReactNative/vtrak/src/components/VolunteerEventList.js
+++ ReactNative/vtrak/src/components/VolunteerEventList.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const VolunteerEventList = () => {
-  return (
-    <View>
-      <Text>Volunteer Event List</Text>
-    </View>
-  );
-};
+class VolunteerEventList extends Component {
+  componentWillMount() {
+     console.log('componentWillMount in Volunteer Event List');
+  }
+  
+  render() {
+    return (
+      <View>
+        <Text>Volunteer Event List</Text>
+      </View>
+    );
+  }
+}
 
 export default VolunteerEventList;
