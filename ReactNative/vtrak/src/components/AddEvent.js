905a55e26e1ebfef8f6dd9f3afd96fbc823d12fb
LYerger <lorraine.yerger@gmail.com>
2018-04-09 18:55:59 -0400

    Styling on Add Event and Login Form

--- ReactNative/vtrak/src/components/AddEvent.js
+++ ReactNative/vtrak/src/components/AddEvent.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import Button from './common/Button';
 import Card from './common/Card';
 import CardSection from './common/CardSection';
 import Input from './common/Input';
+import Header from './header.js';
 
 class AddEvent extends Component {
 	state = { organization: '', date: '', hours: '', notes: '' }
@@ -13,52 +15,55 @@ class AddEvent extends Component {
 
 	render() {
 		return(
-			<Card>
-				<CardSection>
-					<Input
-						label="Event"
-						placeholder="Junior Knights"
-						value={this.state.organization}
-						onChangeText={organization => this.setState({ organization })}
-					/>
-				</CardSection>
+			<View>
+			<Header headerText={'vTrak'} />
+				<Card>
+					<CardSection>
+						<Input
+							label="Event"
+							placeholder="Junior Knights"
+							value={this.state.organization}
+							onChangeText={organization => this.setState({ organization })}
+						/>
+					</CardSection>
 
-				<CardSection>
-					<Input
-						label="Date"
-						placeholder="mm/dd/yyyy"
-						value={this.state.date}
-						onChangeText={date => this.setState({ date })}
-					/>
-				</CardSection>
+					<CardSection>
+						<Input
+							label="Date"
+							placeholder="mm/dd/yyyy"
+							value={this.state.date}
+							onChangeText={date => this.setState({ date })}
+						/>
+					</CardSection>
 
-				<CardSection>
-					<Input
-						label="Hours"
-						placeholder="5"
-						keyboardType="numeric"
-						value={this.state.hours}
-						onChangeText={hours => this.setState({ hours })}
-					/>
-				</CardSection>
+					<CardSection>
+						<Input
+							label="Hours"
+							placeholder="5"
+							keyboardType="numeric"
+							value={this.state.hours}
+							onChangeText={hours => this.setState({ hours })}
+						/>
+					</CardSection>
 
-				<CardSection>
-					<Input
-						multiline
-						label="Notes"
-						placeholder="Taught high schoolers."
-						value={this.state.notes}
-						onChangeText={notes => this.setState({ notes })}
-					/>
-				</CardSection>
+					<CardSection>
+						<Input
+							multiline
+							label="Notes"
+							placeholder="Taught high schoolers."
+							value={this.state.notes}
+							onChangeText={notes => this.setState({ notes })}
+						/>
+					</CardSection>
 
-				<CardSection>
-					<Button
-						title="Add New Event"
-						onPress={this.submitEvent}
-					/>
-				</CardSection>
-			</Card>
+					<CardSection>
+						<Button
+							title="Add New Event"
+							onPress={this.submitEvent}
+						/>
+					</CardSection>
+				</Card>
+			</View>
 		);
 	}
 }
--- ReactNative/vtrak/src/components/LoginForm.js
+++ ReactNative/vtrak/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 
 import Button from './common/Button';
 import Card from './common/Card';
@@ -15,43 +15,60 @@ class LoginForm extends Component {
   render() {
     return (
       <View>
-      <Header headerText={'vTrak'} />
+        <Header headerText={'vTrak'} />
 
-      <Card>
-        <CardSection>
-          <Input
-            label="Username"
-            value={this.state.username}
-            onChangeText={username => this.setState({ username })}
-          />
-        </CardSection>
+        <Card>
+          <CardSection>
+            <Input
+              label="Username"
+              value={this.state.username}
+              onChangeText={username => this.setState({ username })}
+            />
+          </CardSection>
 
-        <CardSection>
-          <Input
-            secureTextEntry
-            label="Password"
-            value={this.state.password}
-            onChangeText={password => this.setState({ password })}
-          />
-        </CardSection>
+          <CardSection>
+            <Input
+              secureTextEntry
+              label="Password"
+              value={this.state.password}
+              onChangeText={password => this.setState({ password })}
+            />
+          </CardSection>
 
-        <CardSection>
+        </Card>
+
+        <View style={styles.buttonContainerStyle}>
+
           <Button onPress={() => Actions.homescreen()} >
-            Log in
+            Log In
           </Button>
-        </CardSection>
 
-        <CardSection>
-          <Button onPress={() => Actions.signup()} >
-            New to vTrak? Sign Up!
-          </Button>
-        </CardSection>
+          <TouchableOpacity>
+            <Text style={styles.textStyle} onPress={() => Actions.signup()} >
+              New to vTrak?
+                <Text style={{fontWeight:'bold', color: '#76CB89'}}> Sign Up!</Text>
+            </Text>
+          </TouchableOpacity>
 
-      </Card>
+        </View>
+
       </View>
 
     );
   }
 }
+
+const styles = StyleSheet.create({
+  buttonContainerStyle: {
+    height: 70,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  textStyle: {
+    paddingTop: 10,
+    alignSelf: 'center',
+    fontSize: 14
+  }
+});
 
 export default LoginForm;
