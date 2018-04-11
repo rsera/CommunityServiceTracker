a00d0b0eb2b9e99a8ca8ecc00528f6144ccd8185
Rachael Sera <rsera@knights.ucf.edu>
2018-04-11 15:37:33 -0400

    removed type from dashboard

--- dashboard.php
+++ dashboard.php
@@ -190,7 +190,7 @@
               <tbody>
 				  <?php
   					$thisUserID = $_SESSION['UserID'];
-  					$sql = "SELECT orgID, orgName, orgWebsite FROM organizations INNER JOIN user ON organizations.orgZip = user.userZip AND user.userID = ".$thisUserID;
+  					$sql = "SELECT orgID, orgName, orgType, orgWebsite FROM organizations INNER JOIN user ON organizations.orgZip = user.userZip AND user.userID = ".$thisUserID;
   					$result = mysqli_query($conn, $sql);
 
   					if (mysqli_num_rows($result) > 0)
