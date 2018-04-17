ec53b73332fc99084a81e8745d308af088de5018
LYerger <lorraine.yerger@gmail.com>
2018-04-17 17:20:00 -0400

    Replaced progress bar with Goal Text

--- ReactNative/vtrak/src/components/HomeScreen.js
+++ ReactNative/vtrak/src/components/HomeScreen.js
@@ -7,6 +7,7 @@ import VolunteerEventList from './VolunteerEventList.js';
 import WelcomeUser from './WelcomeUser';
 import GoalBar from './GoalBar.js';
 import RecentHistoryHeader from './RecentHistoryHeader.js';
+import GoalText from './GoalText.js';
 
 const HomeScreen = () => {
   return (
@@ -14,7 +15,7 @@ const HomeScreen = () => {
       <View style={{backgroundColor: '#F9F9F9'}}>
         <Header headerText={'vTrak'} />
       	<WelcomeUser />
-        <GoalBar />
+        <GoalText />
         <RecentHistoryHeader />
       </View>
       <VolunteerEventList />
